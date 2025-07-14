621a8bef22ef5695ce9fe69c96bcd49a89c0f639
land <glezaland05@gmail.com>
2025-07-14 04:23:06 +0100

    ajout du liens qui emmene a la carte

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,20 +13,17 @@
     window.onscroll = function() {headerShadow()};
 
     function headerShadow() {
-      const navHeader =document.getElementById("header");
+      const navHeader = document.getElementById("header");
+      const isMobile = window.innerWidth <= 540;
 
-      if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
-
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
-        navHeader.style.height = "70px";
-        navHeader.style.lineHeight = "70px";
-
+        navHeader.style.height = isMobile ? "50px" : "70px";
+        navHeader.style.lineHeight = isMobile ? "50px" : "70px";
       } else {
-
         navHeader.style.boxShadow = "none";
-        navHeader.style.height = "90px";
-        navHeader.style.lineHeight = "90px";
-
+        navHeader.style.height = isMobile ? "40px" : "90px";
+        navHeader.style.lineHeight = isMobile ? "40px" : "90px";
       }
     }
 
